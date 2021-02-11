cd4e3446082b6b5034d27f0d81aa9a2c026be30b
Mert AKKAYA <mertakkaya522@gmail.com>
2021-02-11 18:03:16 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,30 @@
+﻿using Core.Data_Access.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Concrete.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetail()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto {ProductId=p.ProductId,CategoryName=c.CategoryName,
+                             ProductName=p.ProductName,UnitsInStock=p.UnitsInStock};
+                return result.ToList();
+            }
+            
+        }
+    }
+}
